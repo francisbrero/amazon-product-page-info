fe2d3f578f9264eab33eba633b4a383dea1086c7
Francis <francis@madkudu.com>
2021-04-11 11:21:27 -0700

    better handling of some corner cases with availability of product

--- lib/info_parse.py
+++ lib/info_parse.py
@@ -52,9 +52,10 @@ with open('./input/sku_list.csv', newline='') as csvfile:
 			avail = ''
 			sold_by = ''
 			ship_by = ''
+			all_options = ''
 
 			try:
-				avail = soup.find_all("div", id="availability".split())[0].text.strip()
+				avail = soup.find_all("div", id="availability".split())[0].text.strip().replace('\n','')
 
 				sold_buy_box = soup.find_all("span", id="tabular-buybox-truncate-0".split())[0]
 				sold_by = sold_buy_box.find("span", class_="tabular-buybox-text").text.strip()
@@ -66,55 +67,48 @@ with open('./input/sku_list.csv', newline='') as csvfile:
 				f.write('"' + sku + '", "' + avail + '", "' + sold_by + '"' +',"' + ship_by + '"\n')
 				cnt =+1
 			except:
-				print("error with SKU " + sku)
-				sku_retry.append(sku)
-				err =+1
-
+				# in some cases there is no seller available and there is only a buybox to show all options => we'll process that separately
+				# let's check to see if there is that buybox option there if so we consider the product not available
+				try:
+					avail = soup.find_all("	", id="buybox-see-all-buying-choices".split())[0].text.strip().replace('\n','')
+					sold_by = 'N/A'
+					ship_by = 'N/A'
+					
+					f.write('"' + sku + '", "' + avail + '", "' + sold_by + '"' +',"' + ship_by + '"\n')
+					cnt =+1
+				except:
+					# in some cases only second hand products are available
+					try:
+						all_options = soup.find_all("div", id="usedbuyBox".split())
+						avail = "Used - "+soup.find_all("div", id="availability".split())[0].text.strip().replace('\n','')
+						sold_by = soup.find_all("a", id="sellerProfileTriggerId".split())[0].text.strip().replace('\n','')
+						ship_by = 'N/A'
+						
+						f.write('"' + sku + '", "' + avail + '", "' + sold_by + '"' +',"' + ship_by + '"\n')
+						cnt =+1
+					except:
+						# in some rare cases, products are completely out of stock
+						try: 
+							all_options = soup.find_all("div", id="outOfStock".split())
+							avail = 'Out of Stock'
+							sold_by = 'N/A'
+							ship_by = 'N/A'
+							
+							f.write('"' + sku + '", "' + avail + '", "' + sold_by + '"' +',"' + ship_by + '"\n')
+							cnt =+1
+						except:
+							print("error with parsing info for SKU " + sku)
+							sku_retry.append(str(sku).strip())
+							err =+1
 		else:
 		    # JavaScript is failed
-		    print("something went wrong with row "+row)
-		    sku_retry.append(sku)
+		    print("something went wrong scraping SKU "+row)
 		    err =+1
-	
-	# Rety on the SKUs where there was an error
-	print("retrying for the " + err + "SKUs that returned an error on first try")
-	err = 0
-	for item in sku_retry:
-		try:
-			result = execute_js('lib/get_info.js '+' '.join(item))
-		except:
-			print('could not scrape')
-		if result:
-		    # JavaScript is successfully executed
-		    # Open file and parse
-			with open('./data/page.html', 'rb') as file:
-			    soup = BeautifulSoup(file,"html.parser")
 
-			# initialize
-			sku = ' '.join(item)
-			avail = ''
-			sold_by = ''
-			ship_by = ''
+print("summary: ")
+print("Processed SKUs: " + str(cnt))
+print("Processed SKUs that returned errors: " + str(err))
 
-			try:
-				avail = soup.find_all("div", id="availability".split())[0].text.strip()
-
-				sold_buy_box = soup.find_all("span", id="tabular-buybox-truncate-0".split())[0]
-				sold_by = sold_buy_box.find("span", class_="tabular-buybox-text").text.strip()
-
-				ship_by_box = soup.find_all("span", id="tabular-buybox-truncate-1".split())[0]
-				ship_by = ship_by_box.find("span", class_="tabular-buybox-text").text.strip()
-
-			
-				f.write('"' + sku + '", "' + avail + '", "' + sold_by + '"' +',"' + ship_by + '"\n')
-			except:
-				print("error with retry for SKU " + sku)
-				err =+1
-
-		else:
-		    # JavaScript is failed
-		    print("something went wrong on retry with SKU " + item)
-		    err =+1
 print(r"""
  ________  ________  ________   _______           ________      ___    ___      ________  ___  ________  ___  ___  ________  ___  ________  ___  ___     
 |\   ___ \|\   __  \|\   ___  \|\  ___ \         |\   __  \    |\  \  /  /|    |\   __  \|\  \|\   __  \|\  \|\  \|\   __  \|\  \|\   __  \|\  \|\  \    
@@ -125,4 +119,3 @@ print(r"""
     \|_______|\|_______|\|__| \|__|\|_______|        \|_______|\___/ /             \|__|     \|__|\|_______|\|_______|\|__|     \|__|\|_______|\|_______|
                                                               \|___|/                                                                                    
                 """)
-print("you're welcome!")
